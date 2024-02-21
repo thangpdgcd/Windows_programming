63756a6e26cc720d1cd298a70646a3cbf9d9be61
Trung <dinhtrung0012@gmail.com>
2024-02-21 16:54:43 +0700

    Them mot button moi o dong 10

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)  // them mot button moi dong 10
+        {
+
+        }
     }
 }
